60fb52e34d170029bd7e8c9162b9366a7b311b27
afpa1701871 <martine.poix@afpa.fr>
2019-06-27 09:21:39 +0200

    version initiale

---
+++ Ajout element à une liste/JS/desserts.js
@@ -0,0 +1,15 @@
+function modifieDessert(e)
+{
+    e.target.textContent = prompt("quel dessert",e.target.textContent);
+}
+function ajoutDessert (){
+    var nouvLi = document.createElement("li");
+    nouvLi.textContent = prompt("quel dessert");
+    nouvLi.addEventListener("click",  modifieDessert );
+    document.getElementById("desserts").appendChild(nouvLi);
+}
+var ajoutBtnElt = document.querySelector("button");
+// Ajout d'un gestionnaire pour l'événement click
+ajoutBtnElt.addEventListener("click", ajoutDessert);
+
+
